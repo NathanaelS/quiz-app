c63b11cdcb1658dead1be0aeed393f212035745b
NathanaelS <Nathanael.stutz@gmail.com>
2015-10-08 21:09:40 -0700

    Added logic to determine correct answer, and other things

--- js/app.js
+++ js/app.js
@@ -40,6 +40,13 @@ $(document).ready(function() {
 
 		];
 
+	var correctAnswers = [
+
+		"answer1",
+		"giggety",
+		"giggety goo"
+	];
+
 		//Like the question part, this will input the first values onto the page
 	$ans1.find("label").text(answerDatabase[0][counter]);
     $ans1.find("input").prop("value", answerDatabase[0][counter]);
@@ -50,18 +57,56 @@ $(document).ready(function() {
 	$ans3.find("label").text(answerDatabase[2][counter]);
     $ans3.find("input").prop("value", answerDatabase[2][counter]);
 
+	
+	// //This starts a counter for the number of right answers
+	var correct = 0;
+
+	//This determines if the answer is correct
+	var checkAnswer = function() {
+		$( "#submit" ).on( "click", function() {
+			if ($("input:checked").val() === correctAnswers[counter]) {
+			$( "#log" ).html( $( "input:checked" ).val() + " is correct!" );
+			//if the answer is correct, this value increases
+			correct++;
+			//if the answer is wrong, this message is displayed
+			} else {$("#log").html("Sorry, that is wrong")};
+			//when the submit button is clicked, the radio buttons are disabled so no new answers can be chosen
+			$('input[name=answer]').attr('disabled', true);
+	})
+	};
+	//Immediately calls the checkAnswer function
+	checkAnswer();
+
+
 	 //Every time that the next button is clicked, this cycles through the array.
+	
+	 var counterAdvance = function() {
+	 	if (counter < answerDatabase.length) {
+	 		counter++;
+	 		console.log("This is a test");
+	 	}
+	 };
+	 
+
+	 // var completion = function() {
+	 // 	if (counter = answerDatabase.length) {
+	 // 		$('#log').html("Congratulations! You had " + correct + " correct!")
+	 // 	};
+	 // }
 	$('#next').click(function () {
 
+
 		//Increases the counter by one and displays the number in the console
-		counter++;
+		counterAdvance();
+		$('input[name=answer]').attr('disabled', false);
 	 	console.log(counter);
+
 
 	 	//Unchecks the radio button and clears the text
 	 	$('input[name=answer]').attr('checked',false);
     	$('#log').text( '' );
 		
-		//Advances the questions and answers the the next values
+		//Advances the questions and answers to the next values
 	 	$('#question').text(questionDatabase[counter]);
     	$ans1.find("label").text(answerDatabase[0][counter]);
     	$ans1.find("input").prop("value", answerDatabase[0][counter]);
@@ -70,13 +115,15 @@ $(document).ready(function() {
     	$ans3.find("label").text(answerDatabase[2][counter]);
     	$ans3.find("input").prop("value", answerDatabase[2][counter]);
     	
-    	
+    	// completion();
 		});
 
+	
+
 	//This displays to the user the answer that they checked 
-	 $( "input" ).on( "click", function() {
-  		$( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
-		});
+	 // $( "input" ).on( "click", function() {
+  // 		$( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
+		// });
 
 
 });
