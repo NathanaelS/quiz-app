73b6949505b4440e004c290c24be093c92ee2751
Nathanael Stutz <stuna@Golden-243.local>
2015-10-17 12:46:10 -0700

    Uploading so I can switch origin

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
+	//This is the counter that is referenced by the next button
 	counter = 0;
 
+	var user = prompt("Welcome! Before we start this quiz, please tell us your name", "");
+
+	console.log(user);
+
+	var checkUser = function() {
+		if (user === "null") {
+			alert("Sorry, that is not a valid name");
+		};
+	};
+
+	checkUser();
+
 	$(document).ready(function() {
-
-	//This starts the counter for our click through function
-	//An option to do here is set it to -1, so the user must click next in order to see the first question upon loading
-
-
 
 	//This is the question database, where the question the student will be asked is stored
 	var questionDatabase = [
@@ -79,21 +87,14 @@
 	//Immediately calls the checkAnswer function
 	checkAnswer();
 
-	 
-	 //I am trying to make a function so when we hit the end of the array, the radio buttons and next button are disabled and it displays the number of correct answers
+	 //This will display the number of correct answers when the quiz is over
 	 var completion = function() {
-	 	// if (counter = answerDatabase.length) {
-	 		$('#log').html("Congratulations! You had " + correct + " correct!");
+	 		alert("Congratulations " + user +"! You had " + correct + " correct!");
 	 		$('input[name=answer]').attr('disabled', 'disabled');
 	 		$('#next').attr('disabled', true);
-	 	// };
 	 };
-
-	 //Every time that the next button is clicked, this cycles through the array.
+	
 	var nextQuestion = function() { 
-
-		// $('#next').click(function () {
-
 
 		//Increases the counter by one and displays the number in the console
 		counter++;
@@ -117,15 +118,14 @@
     	quiz();
 
 		};
-	var number = 10;
 
-	//This is the actual quiz, if the counter is less than the length of the answers array, it advances, if not, it completes
+	//At the end, when the counter equals the answerDatabase array length, it will display the completion funciton
 	var quiz = function() {
 		if ( counter === answerDatabase.length) {
-			console.log("This is getting ridiculous");
 			completion();}
 		};
-	//I call this immediately	
+ 		
+ 	//Every time that the next button is clicked, this cycles through the array.
 		$('#next').click (nextQuestion);
 
 });
