9077fe94959c4236f39e1bb189f7df56717f0e54
NathanaelS <Nathanael.stutz@gmail.com>
2015-11-02 18:45:52 -0700

    random commit

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@
 	
 	//This prints the first value of the questionDatabase to the page
 	$('#question').text(questionDatabase[counter]);
-
+	// document.getElementByid('question')
 	//These are the answer variables
 	var $ans1 = $('#ans1'), $ans2 = $('#ans2'), $ans3 = $('#ans3');
 
